f7fdf680a0064190420093ae7b7f700cecc831b0
Oluwaseyi Salami <osalami09@gmail.com>
2024-06-21 14:46:55 +0000

    Add changes for 'Use Copilot to autocomplete code'

--- TrieDictionary/Trie.cs
+++ TrieDictionary/Trie.cs
@@ -10,7 +10,7 @@ public class TrieNode
         Children = new Dictionary<char, TrieNode>();
         IsEndOfWord = false;
         _value = value;
-    }
+    }        
 
     public bool HasChild(char c)
     {
@@ -25,6 +25,26 @@ public class Trie
     public Trie()
     {
         root = new TrieNode();
+    }
+
+    // Search for a word in the trie
+    public bool Search(string word)
+    {
+        TrieNode current = root;
+        // For each character in the word
+        foreach (char c in word)
+        {
+            // If the current node does not have the character as a child
+            if (!current.HasChild(c))
+            {
+                // The word does not exist in the trie
+                return false;
+            }
+            // Move to the next node
+            current = current.Children[c];
+        }
+        // Return true if the current node marks the end of the word
+        return current.IsEndOfWord;
     }
 
     public bool Insert(string word)
@@ -73,7 +93,17 @@ public class Trie
 
     private List<string> GetAllWordsWithPrefix(TrieNode root, string prefix)
     {
-        return null;
+        List<string> words = new List<string>();
+        if (root.IsEndOfWord)
+        {
+            words.Add(prefix);
+        }
+        foreach (var child in root.Children)
+        {
+            words.AddRange(GetAllWordsWithPrefix(child.Value, prefix + child.Key));
+        }
+
+        return words;
     }
 
     public List<string> GetAllWords()
